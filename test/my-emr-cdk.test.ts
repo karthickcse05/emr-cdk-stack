00e45a9a7ce4b4bec26336f3653c8e32ba748635
skarthick <karthick.s@tranetechnologies.com>
2024-10-10 07:32:37 +0530

    Initial commit

---
+++ test/my-emr-cdk.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as MyEmrCdk from '../lib/my-emr-cdk-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/my-emr-cdk-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new MyEmrCdk.MyEmrCdkStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
